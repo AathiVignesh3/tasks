bf9b913c507746e73c952401529979eda1da88ed
Aathi Vignesh <aathivigesh221@gmail.com>
2022-08-11 17:08:02 +0530

    created task

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+ root "tasks#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
